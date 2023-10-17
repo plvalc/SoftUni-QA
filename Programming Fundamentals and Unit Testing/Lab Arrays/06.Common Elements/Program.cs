e1f0c215e41be7a03b8d655b9dbfa3b79fb456cf
plvalc <123941110+plvalc@users.noreply.github.com>
2023-10-17 15:51:15 +0300

    Add solutions

---
+++ Programming Fundamentals and Unit Testing/Lab Arrays/06.Common Elements/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+foreach (int i in firstArray)
+{
+    foreach (int j in secondArray)
+    {
+        if (i == j)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
