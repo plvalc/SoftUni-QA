bab6f2df84d973084b3cc7e6c00ffc4c842f6798
plvalc <123941110+plvalc@users.noreply.github.com>
2023-11-16 15:29:00 +0200

    New solution

---
+++ Programming Advanced for QA - Nov 2023/Dictionaries, Lambda and LINQ/01.Count Chars in a String/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<char, int> chars = new();
+string input = Console.ReadLine();
+
+foreach (char ch in input)
+{
+    if (ch == ' ')
+        continue;
+    if (!chars.ContainsKey(ch))
+        chars.Add(ch, 1);
+    else
+        chars[ch]++;
+}
+
+foreach (var ch in chars)
+{
+    Console.WriteLine($"{ch.Key} -> {ch.Value}");
+}
