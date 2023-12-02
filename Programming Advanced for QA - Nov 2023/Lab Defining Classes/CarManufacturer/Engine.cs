55b7d9611b631cd1c646f9f97fe5e852955fe358
plvalc <123941110+plvalc@users.noreply.github.com>
2023-12-02 15:55:17 +0200

    Add solution

---
+++ Programming Advanced for QA - Nov 2023/Lab Defining Classes/CarManufacturer/Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+        public int HorsePower {  get; set; }
+        public double CubicCapacity { get; set; }
+    }
+}
