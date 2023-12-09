605d34f21812a79bf2c046c105fcfb38ec1d6c63
plvalc <123941110+plvalc@users.noreply.github.com>
2023-12-09 19:13:58 +0200

    Add solutions

---
+++ Programming Advanced for QA - Nov 2023/Exception Handling/01.Square Root/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (n < 0)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    double sqrt = Math.Sqrt(n);
+                    Console.WriteLine(sqrt);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
---
+++ Programming Advanced for QA - Nov 2023/Exception Handling/02.Enter Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02.Enter_Numbers
+{
+    internal class Program
+    {
+        static int ReadNumber(int start, int end)
+        {
+            int num = 1;
+            num = int.Parse(Console.ReadLine());
+            if (num <= start || num >= end)
+                    throw new ArgumentException();
+
+                return num;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new();
+            int numbersCount = 0;
+            int prevNum = 1;
+            while(numbersCount < 10)
+            {
+                try
+                {
+
+                    int num = ReadNumber(prevNum, 100);
+                    numbers.Add(num);
+                    prevNum = num;
+                    numbersCount++;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"Your number is not in range {prevNum} - 100!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
