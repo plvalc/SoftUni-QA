9b23b407c0a7cdaabdbb8b8212516d144a9d1b5f
plvalc <123941110+plvalc@users.noreply.github.com>
2023-12-09 16:47:35 +0200

    Add solutions

---
+++ Programming Advanced for QA - Nov 2023/10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/ArticleTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class ArticleTests
+{
+    private Article _article;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _article = new Article();
+    }
+
+
+    [Test]
+    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    {
+        // Arrange
+        string[] article = { "Article Content Author", "Article2 Content2 Author2", "Article3 Content3 Author3" };
+
+        // Act
+        Article result = _article.AddArticles(article);
+
+        // Assert
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+    }
+
+    [Test]
+    public void Test_GetArticleList_SortsArticlesByTitle()
+    {
+        // Arrange
+        Article article = new()
+        {
+            ArticleList = new()
+            {
+            new Article {
+            Title = "Some title",
+            Content = "Some content",
+            Author = "Some Author"
+            },
+            new Article
+            {
+            Title = "Any title",
+            Content = "Any content",
+            Author = "Any Author"
+            }
+            }
+        };
+        string expected = $"Any title - Any content: Any Author{Environment.NewLine}Some title - Some content: Some Author";
+
+        // Act
+        string result = this._article.GetArticleList(article, "title");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+    {
+        // Arrange
+        Article article = new()
+        {
+            ArticleList = new()
+            {
+            new Article {
+            Title = "Some title",
+            Content = "Some content",
+            Author = "Some Author"
+            },
+            new Article
+            {
+            Title = "Any title",
+            Content = "Any content",
+            Author = "Any Author"
+            }
+            }
+        };
+
+        // Act
+        string result = this._article.GetArticleList(article, "year");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(String.Empty));
+    }
+}
---
+++ Programming Advanced for QA - Nov 2023/10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/VehicleTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+using System;
+
+using TestApp.Vehicle;
+
+namespace TestApp.UnitTests;
+
+public class VehicleTests
+{
+    private Vehicles _vehicles;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._vehicles = new();
+    }
+
+
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsSortedCatalogue()
+    {
+        // Arrange
+        string[] input = { "Car/Ford/Focus/120", "Car/Toyota/Camry/150", "Truck/Volvo/VNL/500" };
+        string expected = $"Cars:{Environment.NewLine}Ford: Focus - 120hp" +
+            $"{Environment.NewLine}Toyota: Camry - 150hp" +
+            $"{Environment.NewLine}Trucks:{Environment.NewLine}Volvo: VNL - 500kg";
+
+        // Act
+        string result = this._vehicles.AddAndGetCatalogue(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsEmptyCatalogue_WhenNoDataGiven()
+    {
+        // Arrange
+        string[] input = { };
+
+        // Act
+        string result = this._vehicles.AddAndGetCatalogue(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"Cars:{Environment.NewLine}Trucks:"));
+    }
+}
