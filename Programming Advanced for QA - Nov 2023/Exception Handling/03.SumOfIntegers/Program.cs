68c18ceb1b10e6e0ab42d73653149571e904920b
Plamen Valchev <plamenv@gmail.com>
2023-12-10 13:45:40 +0200

    Add solutions

---
+++ Programming Advanced for QA - Nov 2023/Exception Handling/03.SumOfIntegers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+namespace _03.SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] allElements = input.Split();
+            int sumOfIntegers = 0;
+            int currentNum = 0;
+            string currentElement = "";
+
+            for (int i = 0; i < allElements.Length; i++)
+            {
+                try
+                {
+                    currentElement = allElements[i];
+                    currentNum = int.Parse(currentElement);
+                    sumOfIntegers += currentNum;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{currentElement}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{currentElement}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{currentElement}' processed - current sum: {sumOfIntegers}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sumOfIntegers}");
+        }
+    }
+}
