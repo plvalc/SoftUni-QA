2d919b6c11db87a5ec829068a2aa5e589ffbac68
plvalc <123941110+plvalc@users.noreply.github.com>
2023-12-13 14:16:46 +0200

    Add solutions

---
+++ Programming Advanced for QA - Nov 2023/13-Resources-OOP-Principles-Encapsulation-Inheritance/BoxData/Box.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace BoxData;
+
+public class Box
+{
+    private double _length;
+    private double _width;
+    private double _height;
+    public Box(double length, double width, double height)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+    }
+
+    public double Length 
+    { 
+        get
+        {
+            return this._length;
+        }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException($"Length cannot be zero or negative.");
+            this._length = value;
+        }
+    }
+    public double Width
+    {
+        get
+        {
+            return this._width;
+        }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException($"Width cannot be zero or negative.");
+            this._width = value;
+        }
+    }
+    public double Height
+    {
+        get
+        {
+            return this._height;
+        }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException($"Height cannot be zero or negative.");
+            this._height = value;
+        }
+    }
+    
+    public double SurfaceArea()
+    {
+        double area = 2 * Length * Width + 2 * Length * Width + 2 * Width * Height;
+        return area;
+    }
+    public double Volume()
+    {
+        return this.Length * this.Width * this.Height;
+    }
+    public override string ToString()
+    {
+        return $"Surface Area – {this.SurfaceArea():F2}{Environment.NewLine}Volume – {Volume():F2}";
+    }
+
+}
---
+++ Programming Advanced for QA - Nov 2023/13-Resources-OOP-Principles-Encapsulation-Inheritance/PersonInfo/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PersonInfo;
+
+public class Person
+{
+    public Person(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+
+    private const int NAME_MIN_LENGTH = 3;
+
+    public string FirstName 
+    {
+        get
+        {
+            return this._firstName;
+        }
+        set
+        {
+            if (value.Length < NAME_MIN_LENGTH)
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            this._firstName = value;
+        } 
+    }
+    public string LastName
+    {
+        get
+        {
+            return this._lastName;
+        }
+        set
+        {
+            if (value.Length < NAME_MIN_LENGTH)
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            this._lastName = value;
+        }
+    }
+    public int Age 
+    { 
+        get
+        {
+            return this._age;
+        }
+        set 
+        {
+            if (value <= 0)
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            this._age = value;
+
+        } 
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+    }
+}
